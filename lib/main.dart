43e650d8ec784570b92de1c4e5180c5b3e174928
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-11-08 14:23:41 +0100

    maps and routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 // import 'pages/home.dart';
 import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+import 'package:world_time/pages/choose_location.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: const Home()
+    // home: const Home(),
+    initialRoute: '/home', // setting an initial screen for testing purposes
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation()
+    }
   ));
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,8 +10,20 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text('home screen'))
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              TextButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text('Edit Location')
+              )
+            ],
+          )
+      )
     );
   }
 }
