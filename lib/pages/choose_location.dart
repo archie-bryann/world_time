af47086870213213d253861e2ab2cfc994b59704
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-11-08 15:49:19 +0100

    widget lifecycle

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ChooseLocation extends StatefulWidget {
@@ -8,8 +9,22 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    if (kDebugMode) {
+      print('initState function ran');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (kDebugMode) {
+      print('build function ran');
+    }
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -19,7 +34,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('choose location screen')
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Text('counter is $counter')
+      )
     );
   }
 }
