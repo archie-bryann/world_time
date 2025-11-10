511def1f02b4fd93ee5c75ad8b544098f747fe5d
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-11-10 15:49:03 +0100

    error handling

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,29 +11,36 @@ class WorldTime {
   WorldTime({ required this.location, required this.flag, required this.url });
 
   Future<void> getTime() async {
-    Response response = await get(
-        Uri.parse('https://worldtimeapi.org/api/timezone/$url')
-    );
-    Map data = jsonDecode(response.body);
-    if (kDebugMode) {
-      print(data);
+    try {
+      Response response = await get(
+          Uri.parse('https://worldtimeapi.org/api/timezone/$url')
+      );
+      Map data = jsonDecode(response.body);
+      if (kDebugMode) {
+        print(data);
+      }
+
+      // get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      // if(kDebugMode) {
+      //   print(datetime);
+      //   print(offset);
+      // }
+
+      // create DateTime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      // if(kDebugMode) {
+      //   print(now);
+      // }
+      time = now.toString();
+    } catch (e) {
+      if (kDebugMode) {
+        print('caught error: $e');
+        time = "an error occurred...";
+      }
     }
-
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    // if(kDebugMode) {
-    //   print(datetime);
-    //   print(offset);
-    // }
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    // if(kDebugMode) {
-    //   print(now);
-    // }
-    time = now.toString();
   }
 }
 
