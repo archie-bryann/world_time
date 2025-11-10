7d28bdc32025aebc04337633ed1b53d97d75f4f9
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-11-10 14:39:38 +0100

    worldtime custom class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -12,6 +11,19 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  String time = 'loading...';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: "Berlin", flag: "germany.png", url: "Europe/Berlin");
+    await instance.getTime();
+    if (kDebugMode) {
+      print(instance.time);
+      setState(() {
+        time = instance.time;
+      });
+    }
+  }
+
   // void getData() async {
   //   Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
   //   Map data = jsonDecode(response.body);
@@ -21,42 +33,46 @@ class _LoadingState extends State<Loading> {
   //   }
   // }
   
-  void getTime() async {
-    Response response = await get(
-      Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London')
-    );
-    Map data = jsonDecode(response.body);
-    if (kDebugMode) {
-      print(data);
-    }
-
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    // if(kDebugMode) {
-    //   print(datetime);
-    //   print(offset);
-    // }
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    if(kDebugMode) {
-      print(now);
-    }
-  }
+  // void getTime() async {
+  //   Response response = await get(
+  //     Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London')
+  //   );
+  //   Map data = jsonDecode(response.body);
+  //   if (kDebugMode) {
+  //     print(data);
+  //   }
+  //
+  //   // get properties from data
+  //   String datetime = data['datetime'];
+  //   String offset = data['utc_offset'].substring(1, 3);
+  //   // if(kDebugMode) {
+  //   //   print(datetime);
+  //   //   print(offset);
+  //   // }
+  //
+  //   // create DateTime object
+  //   DateTime now = DateTime.parse(datetime);
+  //   now = now.add(Duration(hours: int.parse(offset)));
+  //   if(kDebugMode) {
+  //     print(now);
+  //   }
+  // }
 
   @override
   void initState() {
     super.initState();
     // getData();
-    getTime();
+    // getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('loading screen'),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
